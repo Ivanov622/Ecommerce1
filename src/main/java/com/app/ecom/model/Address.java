65ae325de690b1e322710c7e0390ab76cab9b7a3
Ivan Martinez Ardila <imartineza@gmail.com>
2026-04-20 15:48:09 -0500

    Se adiciona funcionamiento con Jpa y H2

---
+++ src/main/java/com/app/ecom/model/Address.java
@@ -0,0 +1,25 @@
+package com.app.ecom.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Entity(name = "addresses")
+public class Address {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public Long id;
+	public String street;
+	public String city;
+	public String state;
+	public String country;
+	public String zipcode;
+	
+	
+	
+
+}
